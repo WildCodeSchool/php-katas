ef9494c53fdc451731b5011431c8881f043d708a
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-09-29 11:38:56 +0200

    add tester

---
+++ tester.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+clear
+if [[ -n "$1" ]]; then
+    classname="$1"
+    vendor/bin/phpunit src/Tests/${classname}Test.php
+else
+    echo "Please specify the kata to test, e.g. : ./tester.sh HelloYou"
+fi
