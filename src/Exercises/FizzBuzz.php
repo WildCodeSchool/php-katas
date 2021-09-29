d833ddb9c57a53a6fc18f93fa9c6d111f5948eb0
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-09-29 11:59:49 +0200

    edit FizzBuzz

--- src/Exercises/FizzBuzz.php
+++ src/Exercises/FizzBuzz.php
@@ -12,17 +12,4 @@ namespace Wcs\Katas\Exercises;
 class FizzBuzz
 {
     // TODO
-    function fizzBuzz($n)
-    {
-        if ($n % 15 === 0) {
-            return "FizzBuzz";
-        }
-        if ($n % 3 === 0) {
-            return "Fizz";
-        }
-        if ($n % 5 === 0) {
-            return "Buzz";
-        }
-        return "$n";
-    }
 }
