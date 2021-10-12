6ac6e8951d5751e6ed552601e8267d1f649dd2c2
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-10-12 08:56:20 +0200

    add FoodQueries

---
+++ src/FoodQueries/FoodQueries.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\FoodQueries;
+
+/**
+ * Ecrire les requêtes suivantes :
+ *
+ * Créer une table food contenant les champs suivants :
+ * - id (clé primaire auto incrémentée)
+ * - nom de l'aliment (maximum 100 caractères)
+ * - numéro de série unique
+ * - nombre de calories pour 100gr
+ *
+ * Ajouter des aliments à la table
+ *
+ * Faire une requête permettant d'afficher tous les aliments
+ *
+ * Faire une requête pour replacer le nom d'un aliment par "tacos"
+ *
+ * Faire une requête affichant les aliments commençants par la lettre "t"
+ *
+ * Faire une requête pour remplacer le nombre de calories de l'aliment "tacos" par la valeur 500
+ *
+ * Faire une requête permettant de récupérer les aliments dont le nombre de calorie est de 200 ou plus
+ *
+ * Faire une requête permettant d'afficher tous les aliments classés par ordre alphabétique
+ *
+ * Faire une requête permettant d'afficher l'aliment le plus calorique
+ *
+ * Faire une requête permettant de supprimer l'aliment trouvé par la requête précédente
+ *
+ */
+class FoodQueries
+{
+    // TODO
+}
