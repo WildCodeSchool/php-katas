abff7ca0fb156f31247af07449e56a4cccf95465
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-11-09 08:52:33 +0100

    add Palindrome

---
+++ src/Palindrome/Palindrome.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Palindrome;
+
+/*
+ * A palindrome is a word or a phrase that reads the same backwards as forwards, example. madam.
+ * Create a function `isPalindrome` that returns if a word is a palindrome.
+ * This function must not be case sensitive.
+ *
+ * Bonus: If the word is null or empty, throw an exception
+ *
+ * Example:
+ * - "rotor" -> true
+ * - "tacos" -> false
+ * - "Kayak" -> true
+ *
+ * Add you own tests.
+ */
+class Palindrome
+{
+    // TODO
+}
---
+++ src/Palindrome/PalindromeTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Palindrome;
+
+use App\Palindrome\Palindrome;
+use PHPUnit\Framework\TestCase;
+
+class PalindromeTest extends TestCase
+{
+    public function testFizzBuzz()
+    {
+        $palindrome = new Palindrome();
+        self::assertEquals(true, true);
+        // TODO: add your own test
+    }
+}
