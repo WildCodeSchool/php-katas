6b3668053021cf5ec0544f16028f23872759a18b
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-11-09 08:58:49 +0100

    add Tennis kata

---
+++ src/TennisScore/TennisScoreTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\TennisScore;
+
+use App\TennisScore\TennisScore;
+use PHPUnit\Framework\TestCase;
+
+class TennisScoreTest extends TestCase
+{
+    public function testFizzBuzz()
+    {
+        $tennisScore = new TennisScore();
+        self::assertEquals(true, true);
+        // TODO: add your own test
+    }
+}
