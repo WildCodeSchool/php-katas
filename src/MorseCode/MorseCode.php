193276f2e2332e6b864401307c65d7f9b7d795c4
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-11-16 08:40:57 +0100

    add kata morse

---
+++ src/MorseCode/MorseCode.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\MorseCode;
+
+/**
+ * Create a function `decode` which translates morse code into latin alphabet.
+ * Morse code is consisting of dots and dashes:
+ * - each word being separated by three spaces
+ * - each letter being separated by one space
+ */
+class MorseCode
+{
+    // TODO
+}
---
+++ src/MorseCode/MorseCodeTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\MorseCode;
+
+use App\MorseCode\MorseCode;
+use PHPUnit\Framework\TestCase;
+
+class TennisScoreTest extends TestCase
+{
+    public function testWorks()
+    {
+        self::assertTrue(true);
+    }
+
+    // TODO: add your own test
+}
