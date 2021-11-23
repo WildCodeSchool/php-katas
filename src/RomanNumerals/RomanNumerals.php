e97bf2f60738ef81909c18826f2209d9693e5a26
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-11-23 10:36:43 +0100

    fix

--- src/RomanNumerals/RomanNumerals.php
+++ src/RomanNumerals/RomanNumerals.php
@@ -23,13 +23,13 @@ namespace App\RomanNumerals;
 class RomanNumerals
 {
     private array $romanNumerals = [
-        "I" => 1,
-        "V" => 5,
-        "X" => 10,
-        "L" => 50,
-        "C" => 100,
-        "D" => 500,
-        "M" => 1000,
+        'I' => 1,
+        'V' => 5,
+        'X' => 10,
+        'L' => 50,
+        'C' => 100,
+        'D' => 500,
+        'M' => 1000,
     ];
 
     // TODO
