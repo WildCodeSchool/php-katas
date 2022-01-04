aa902d0adca0cf8f83f9f65a4f60c178abf8dfd6
bastienwcs <bastien.krettly@wildcodeschool.com>
2022-01-04 08:48:42 +0100

    fix quotes

--- src/TicTacToe/TicTacToe.php
+++ src/TicTacToe/TicTacToe.php
@@ -4,22 +4,22 @@ namespace App\TicTacToe;
 
 /**
  * Create a function `winner` which analyses a 3x3 tic-tac-toe grid.
- * - "X" represents player X
- * - "0" represents player 0
- * - " " (space) is an empty cell
+ * - 'X' represents player X
+ * - '0' represents player 0
+ * - ' ' (space) is an empty cell
  *
  * grid :
  * [
- *   ["X", "O", "O"],
- *   ["X", "O", "O"],
- *   ["X", " ", " "],
+ *   ['X', 'O', 'O'],
+ *   ['X', 'O', 'O'],
+ *   ['X', ' ', ' '],
  * ]
  *
  * The function should returns the winner :
- * - "X wins" when X wins
- * - "O wins" when O wins
- * - "Cat's game" if it's a draw or if nobody wins
- * - "{player} cheated" where {player} is replaced by the player who cheated.
+ * - 'X wins' when X wins
+ * - 'O wins' when O wins
+ * - 'Cat's game' if it's a draw or if nobody wins
+ * - '{player} cheated' where {player} is replaced by the player who cheated.
  *
  * The function should throw :
  * - RangeException if the grid has not the size expected
--- src/TicTacToe/TicTacToeTest.php
+++ src/TicTacToe/TicTacToeTest.php
@@ -7,39 +7,39 @@ use PHPUnit\Framework\TestCase;
 class TicTacToeTest extends TestCase
 {
     private array $xWins = [
-        ["O", "X", "X"],
-        ["O", "X", "O"],
-        ["X", "O", "X"],
+        ['O', 'X', 'X'],
+        ['O', 'X', 'O'],
+        ['X', 'O', 'X'],
     ];
 
     private array $oWins = [
-        ["X", "O", "X"],
-        ["O", "O", "O"],
-        [" ", " ", "X"],
+        ['X', 'O', 'X'],
+        ['O', 'O', 'O'],
+        [' ', ' ', 'X'],
     ];
 
     private array $catsGame = [
-        ["O", "X", "O"],
-        ["X", "X", "O"],
-        ["O", "O", "X"],
+        ['O', 'X', 'O'],
+        ['X', 'X', 'O'],
+        ['O', 'O', 'X'],
     ];
 
     private array $xCheated = [
-        ["O", "X", "X"],
-        ["X", "X", "O"],
-        ["X", "O", "X"],
+        ['O', 'X', 'X'],
+        ['X', 'X', 'O'],
+        ['X', 'O', 'X'],
     ];
 
     private array $rangeException = [
-        ["O", "X", "X"],
-        ["X", "X"],
-        ["X", "O", "X"],
+        ['O', 'X', 'X'],
+        ['X', 'X'],
+        ['X', 'O', 'X'],
     ];
 
     private array $illegalArgument = [
-        ["O", "X", "X"],
-        ["O", "X", "0"],
-        ["X", "O", "X"],
+        ['O', 'X', 'X'],
+        ['O', 'X', '0'],
+        ['X', 'O', 'X'],
     ];
 
     public function testDefault()
